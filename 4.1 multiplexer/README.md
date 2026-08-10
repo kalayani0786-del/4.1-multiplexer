9e9b8e0e24646a634bd6b5099969e832e77b2c2a
kalayani0786-del <kalayani0786@gmail.com>
2026-08-10 19:26:35 +0530

    Add files via upload

---
+++ 4.1 multiplexer/README.md
@@ -0,0 +1,9 @@
+A 4:1 Multiplexer (MUX) is a combinational logic circuit that selects one of four input signals and sends the selected signal to a single output.
+
+It has:
+
+4 data inputs: I0, I1, I2, I3
+2 select lines: S1, S0
+1 output: Y
+
+The select lines determine which input is connected to the output.
---
+++ 4.1 multiplexer/mux4to1.v
@@ -0,0 +1,20 @@
+module mux4to1 (
+    input  I0,
+    input  I1,
+    input  I2,
+    input  I3,
+    input  S1,
+    input  S0,
+    output reg Y
+);
+
+always @(*) begin
+    case ({S1, S0})
+        2'b00: Y = I0;
+        2'b01: Y = I1;
+        2'b10: Y = I2;
+        2'b11: Y = I3;
+    endcase
+end
+
+endmodule
